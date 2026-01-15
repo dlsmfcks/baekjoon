342edf3f5f0583070982546acc48396ad64a774f
Minjae Lee <coris0907@gmail.com>
2026-01-15 16:14:39 +0900

    solve 2439

---
+++ .idea/baekjoon2439/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=0;i<a;i++) {
+            for(int j=1;j<=a-i-1;j++) {
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
