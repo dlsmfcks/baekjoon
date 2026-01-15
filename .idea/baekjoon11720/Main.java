260089af808e403609be2bdbdddc147177cd9e30
Minjae Lee <coris0907@gmail.com>
2026-01-15 16:29:01 +0900

    solve 11720

---
+++ .idea/baekjoon11720/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String a = sc.next();
+        int total = 0;
+        for(int i=0; i< a.length();i++) {
+            total += a.charAt(i) - '0';
+        }
+        System.out.println(total);
+    }
+}
